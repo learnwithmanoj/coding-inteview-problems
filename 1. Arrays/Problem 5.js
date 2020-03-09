84e58c800fa973109d48b23d06466d2bbab74872
Manoj Ganesan Thangaraj <manoj_thangaraj01@blreqx000033lt.ad.infosys.com>
2020-03-09 17:17:00 +0530

    Added smallest difference pair problem

---
+++ 1. Arrays/Problem 5.js
@@ -0,0 +1,7 @@
+/* Smallest difference pair of values between two unsorted Arrays */
+
+function smallest_difference(arrayOne, arrayTwo) {
+
+}
+
+console.log(`Smallest Difference Pair: ${smallest_difference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])}`);
